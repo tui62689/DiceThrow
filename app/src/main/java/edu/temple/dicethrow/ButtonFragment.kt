1bea972194be8c43f73e5f2f102d561ad54690ce
tui62689 <112034701+tui62689@users.noreply.github.com>
2024-10-22 15:07:24 -0400

    Button onclicklistener

--- app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class ButtonFragment : Fragment() {
@@ -15,7 +16,11 @@ class ButtonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false)
+        return inflater.inflate(R.layout.fragment_button, container, false).apply{
+            findViewById<Button>(R.id.button).setOnClickListener{
+
+            }
+        }
     }
 
 }
